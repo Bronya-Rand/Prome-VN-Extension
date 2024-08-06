16530f99380fa12b262dd582fd8dd06cec009116
Bronya-Rand <azarieldc@gmail.com>
2024-08-06 18:58:08 -0500

    feat: initial prome commit

--- README.md
+++ README.md
@@ -1,33 +1,30 @@
-# SillyTavern Extension Example
+# Prome Visual Novel Extension
 
-*Provide a brief description of how your extension works, what problem it aims to solve.*
+The Prome Visual Novel Extension is a extension for SillyTavern that remodernizes the Visual Novel system by separating it from ST itself into a extension.
+
+## Why Prome?
+> Prometheus: Boom...
 
 ## Features
 
-*Describe some of the main selling points of your extension.*
+1. Same features as the original VN mode.
+2. Letterbox Mode - Make your VN experience more 'cinematic.'
+
+## Prerequisites
+
+SillyTavern 1.12.4+
 
 ## Installation and Usage
 
 ### Installation
 
-*In most cases, this should just be using ST's inbuilt extension installer.* 
+1. Click _Extensions_ then **Install Extension**
+2. Paste in the following **link** into the text field and click Save: `https://github.com/Bronya-Rand/Bronie-Parser-Extension`.
+3. Click down on the `Prome (Visual Novel Extension)` dropdown and toggle *Enable Prome VN Mode*. 
+4. Profit.
 
 ### Usage
 
-*Explain how to use this extension.*
+Letterbox Mode - Adds a more cinematic experience to the Visual Novel UI by adding black bars to the background.
+> This can be toggled to be horizontal, vertical or none at all. It can be size and color adjusted as needed.
 
-## Prerequisites
-
-*Specify the version of ST necessary here.*
-
-## Support and Contributions
-
-*Where should someone ask for support?*
-
-*Consider including your own contact info for help/questions.*
-
-*How can people help add to this extension?*
-
-## License
-
-*Be cool, use an open source license.*
--- index.js
+++ index.js
@@ -1,63 +1,152 @@
-// The main script for the extension
-// The following are examples of some basic extension functionality
-
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
-import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
-
-//You'll likely need to import some other functions from the main script
+import { extension_settings } from "../../../extensions.js";
 import { saveSettingsDebounced } from "../../../../script.js";
+import { power_user } from "../../../power-user.js";
+import { isMobile } from "../../../RossAscends-mods.js";
 
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
+const extensionName = "prome-vn-extension";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-const extensionSettings = extension_settings[extensionName];
-const defaultSettings = {};
+const defaultSettings = {
+  enableVN_UI: false,
+  letterboxMode: 0,
+  letterboxColor: "rgba(0, 0, 0, 1)",
+  letterboxSize: 8,
+};
 
+const VN_MODES = {
+  NONE: 0,
+  HORIZONTAL: 1,
+  VERTICAL: 2,
+};
 
- 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
 async function loadSettings() {
-  //Create the settings if they don't exist
   extension_settings[extensionName] = extension_settings[extensionName] || {};
   if (Object.keys(extension_settings[extensionName]).length === 0) {
     Object.assign(extension_settings[extensionName], defaultSettings);
   }
 
-  // Updating settings in the UI
-  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+  // Prome Updates
+  $("#prome-enable-vn").prop("checked", extension_settings[extensionName].enableVN_UI).trigger("input");
+  $("#prome-letterbox-mode").val(extension_settings[extensionName].letterboxMode).trigger("change");
+  $("#prome-letterbox-color-picker").attr('color', extension_settings[extensionName].letterboxColor);
+  $("#prome-letterbox-size").val(extension_settings[extensionName].letterboxSize).trigger("input");
+  $("#prome-letterbox-size-counter").val(extension_settings[extensionName].letterboxSize);
+
+  // ST Main Updates
+  $("#waifuMode").prop("checked", extension_settings[extensionName].enableVN_UI).trigger("input");
+
+  // Apply Letterbox Settings (if enabled)
+  applyLetterboxMode();
+  applyLetterboxColor();
+  applyLetterboxSize();
 }
 
-// This function is called when the extension settings are changed in the UI
-function onExampleInput(event) {
-  const value = Boolean($(event.target).prop("checked"));
-  extension_settings[extensionName].example_setting = value;
+function resetLetterBoxSize() {
+  extension_settings[extensionName].letterboxSize = defaultSettings.letterboxSize;
+  $("#prome-letterbox-size").val(defaultSettings.letterboxSize).trigger("input");
+  $("#prome-letterbox-size-counter").val(defaultSettings.letterboxSize);
   saveSettingsDebounced();
 }
 
-// This function is called when the button is clicked
-function onButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the checked setting
-  toastr.info(
-    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-    "A popup appeared because you clicked the button!"
-  );
+function resetLetterBoxColor () {
+  extension_settings[extensionName].letterboxColor = defaultSettings.letterboxColor;
+  $("#prome-letterbox-color-picker").attr('color', defaultSettings.letterboxColor);
+
+  saveSettingsDebounced();
+}
+
+// Changes the size of the letterbox
+function onLetterboxSize_Change() {
+  const value = this.value;
+  extension_settings[extensionName].letterboxSize = value;
+  $("#prome-letterbox-size-counter").val(value);
+  saveSettingsDebounced();
+  applyLetterboxSize();
+}
+
+// Changes the color of the letterbox
+function onLetterboxColor_Change(evt) {
+  const value = evt.detail.rgba;
+  extension_settings[extensionName].letterboxColor = value;
+  saveSettingsDebounced();
+  applyLetterboxColor();
+}
+
+function applyLetterboxColor() {
+  document.documentElement.style.setProperty('--prom-letterbox-color', extension_settings[extensionName].letterboxColor);
+}
+
+function applyLetterboxSize() {
+  let direction = "vw";
+  if (extension_settings[extensionName].letterboxMode == VN_MODES.HORIZONTAL) {
+    direction = "vh";
+  }
+  document.documentElement.style.setProperty('--prom-letterbox-size', `${extension_settings[extensionName].letterboxSize}${direction}`);
+}
+
+// Enables/Disables Letterbox Mode
+function applyLetterboxMode() {
+  if (extension_settings[extensionName].letterboxMode === (null || undefined)) {
+    console.debug(`[${extensionName}] letterboxMode returned null or undefined.`);
+  }
+
+  console.debug(`[${extensionName}] Letterbox Mode: ${extension_settings[extensionName].letterboxMode}`);
+
+  $('body').toggleClass('hLetterBox', extension_settings[extensionName].letterboxMode === VN_MODES.HORIZONTAL);
+  $('body').toggleClass('vLetterBox', extension_settings[extensionName].letterboxMode === VN_MODES.VERTICAL);
+
+  if (isLetterboxModeEnabled()) {
+    $('#visual-novel-letterbox-one').show();
+    $('#visual-novel-letterbox-two').show();
+  } else {
+    $('#visual-novel-letterbox-one').hide();
+    $('#visual-novel-letterbox-two').hide();
+  }
+
+  applyLetterboxSize();
+
+  console.debug(`[${extensionName}] Letterbox Settings Applied`);
+}
+
+function isLetterboxModeEnabled() {
+  return Boolean(extension_settings[extensionName].letterboxMode !== VN_MODES.NONE);
+}
+
+function isVNMode() {
+  return Boolean(!isMobile() && extension_settings[extensionName].enableVN_UI);
+}
+
+// Selects the Letterbox Mode
+function onLetterbox_Select() {
+  const value = Number(this.value);
+  extension_settings[extensionName].letterboxMode = value;
+  saveSettingsDebounced();
+  applyLetterboxMode();
+}
+
+// Toggles Prome VN Mode
+function onVNUI_Click(event) {
+  const value = Boolean($(event.target).prop("checked"));
+  extension_settings[extensionName].enableVN_UI = value;
+  power_user.waifuMode = value;
+  saveSettingsDebounced();
+
+  // Hijack ST Main's Waifu Mode
+  $('body').toggleClass('waifuMode', power_user.waifuMode);
+  $('#waifuMode').prop('checked', power_user.waifuMode);
 }
 
 // This function is called when the extension is loaded
 jQuery(async () => {
-  // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+  const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
 
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
   $("#extensions_settings").append(settingsHtml);
 
-  // These are examples of listening for events
-  $("#my_button").on("click", onButtonClick);
-  $("#example_setting").on("input", onExampleInput);
+  $("#prome-enable-vn").on("click", onVNUI_Click);
+  $("#prome-letterbox-mode").on("change", onLetterbox_Select);
+  $("#prome-letterbox-color-picker").on("change", onLetterboxColor_Change);
+  $("#prome-letterbox-size").on("input", onLetterboxSize_Change);
+  $("#prome-letterbox-size-restore").on("click", resetLetterBoxSize);
+  $("#prome-letterbox-color-restore").on("click", resetLetterBoxColor);
 
-  // Load settings when starting things up (if you have any)
   loadSettings();
 });
